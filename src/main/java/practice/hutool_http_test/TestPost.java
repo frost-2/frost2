6c77dc8149d3b25f507a9830761e1eaca8f36e5e
frost-2 <59042444+frost-2@users.noreply.github.com>
2020-04-01 18:21:51 +0800

    [!] 添加hutool工具包，测试hutool-http发post请求

---
+++ src/main/java/practice/hutool_http_test/TestPost.java
@@ -0,0 +1,29 @@
+package practice.hutool_http_test;
+
+import cn.hutool.core.lang.Console;
+import cn.hutool.http.HttpUtil;
+
+/**
+ * @author 陈伟平
+ * @date 2020-04-01-上午 11:17
+ */
+public class TestPost {
+
+    public static void post(){
+        String url = "http://localhost:9527/admin/login";
+        String requestBody = "{\"phone\":\"13000000000\", \"password\": \"123456\"}";
+
+        String responseBody = HttpUtil.createPost(url)      //创建一个post请求
+                .body(requestBody)                          //添加请求体
+                .header("dz-usr","1")          //添加请求头
+                .execute()                                  //执行请求
+                .body();                                    //获取响应体内容
+
+        Console.log(responseBody);
+    }
+
+    public static void main(String[] args) {
+        post();
+    }
+
+}
